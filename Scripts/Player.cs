9aec76be0f83d388a39145d44281135a40790c65
CChiverton <75337792+CChiverton@users.noreply.github.com>
2024-10-11 04:31:50 +0100

    Player-Enemy collision
    Player is knocked back when colliding with an enemy.

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,30 +4,57 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	private const float _speed = 100.0f;
+	private const float _knockbackSpeed = 200.0f;
+	private Vector2 _knockback = Vector2.Zero;
+	
+	private void OnKnockbackTimerTimeout()
+	{
+		_knockback = Vector2.Zero;
+	}
+	
+	private void CollisionHandler()
+	{
+		Timer knockbackTimer = GetNode<Timer>("KnockbackTimer");
+		if (knockbackTimer.IsStopped())
+		{
+			for (int i = 0; i < GetSlideCollisionCount(); i++)
+			{
+				Node2D collision = GetSlideCollision(i).GetCollider() as Node2D;
+				if (collision.IsInGroup("Enemies"))
+				{
+					_knockback = -Position.DirectionTo(collision.GlobalPosition) * _knockbackSpeed;
+					GetNode<Timer>("KnockbackTimer").Start();
+				} 
+			}
+		}
+	}
 	
 	private void Movement()
 	{
-		Vector2 Speed = Velocity;
+		Vector2 velocity = Velocity;
 
 		// Get the input direction and handle the movement/deceleration.
-		Vector2 Direction = Input.GetVector("MoveLeft", "MoveRight", "MoveUp", "MoveDown");
-		if (Direction != Vector2.Zero)
+		Vector2 direction = Input.GetVector("MoveLeft", "MoveRight", "MoveUp", "MoveDown");
+		if (direction != Vector2.Zero)
 		{
-			Speed.X = Direction.X * _speed;
-			Speed.Y = Direction.Y * _speed;
+			velocity.X = direction.X * _speed;
+			velocity.Y = direction.Y * _speed;
 		}
 		else
 		{
-			Speed.X = Mathf.MoveToward(Velocity.X, 0, _speed);
-			Speed.Y = Mathf.MoveToward(Velocity.Y, 0, _speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, _speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, _speed);
 		}
 
-		Velocity = Speed;
-		MoveAndSlide();
+		Velocity = velocity + _knockback;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Movement();
+		if (MoveAndSlide())
+		{
+			CollisionHandler();
+		}
 	}
 }
