89cf7545f1b102653178781bb8acb2f3a9e37678
CChiverton <75337792+CChiverton@users.noreply.github.com>
2024-10-11 17:52:45 +0100

    Enemy spawn location randomised
    Enemies now spawn on floor tiles, which required changing the ID and separating it from wall tiles.

--- Scripts/EnemySpawner.cs
+++ Scripts/EnemySpawner.cs
@@ -9,10 +9,21 @@ public partial class EnemySpawner : Node2D
 	
 	private int _maxEnemyCount = 10;
 	
+	private TileMapLayer _map;
+	private Godot.Collections.Array<Vector2I> _spawnableTiles = new Godot.Collections.Array<Vector2I>();
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_enemiesGroup = GetNode<Node2D>("Enemies");
+		_map = GetNode<TileMapLayer>("../TileMapLayer");
+		_spawnableTiles = _map.GetUsedCellsById(0);
+	}
+	
+	private Vector2 SetSpawnLocation()
+	{
+		// TODO Add logic to spawn a a minimum and maximum distance away from the character
+		return _map.MapToLocal(_spawnableTiles.PickRandom());
 	}
 	
 	private void SpawnEnemy()
@@ -20,6 +31,7 @@ public partial class EnemySpawner : Node2D
 		if (_enemiesGroup.GetChildCount() < _maxEnemyCount)
 		{
 			Enemy enemy = _enemy.Instantiate<Enemy>();
+			enemy.GlobalPosition = SetSpawnLocation();
 			_enemiesGroup.AddChild(enemy);
 		}
 	}
@@ -28,6 +40,7 @@ public partial class EnemySpawner : Node2D
 	{
 		SpawnEnemy();
 	}
+	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
